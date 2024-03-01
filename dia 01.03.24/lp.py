48854870226332ceb81667f2f671004bd417aca0
Arthur Medeiros Pinheiro <20231011110020@ifrn.local>
2024-03-01 16:00:59 -0300

    message

---
+++ dia 01.03.24/lp.py
@@ -0,0 +1,20 @@
+"""n = input("Qual seu nome completo?")
+print("Bem-vindo ao Python,",n)"""
+
+
+# n1 = float(input("nota 1 bimestre"))
+# n2 = float(input("nota 2 bimestre"))
+
+# m = (n1 + n2) /2
+
+# print(m)
+
+b = float(input("base"))
+h = float(input("altura"))
+
+a = b * h
+p = (b+b) + (h+h)
+d = (a**2 + b**2) **0.5
+print(a)
+print(p)
+print(f"a diagonal é {d:.2f}")
