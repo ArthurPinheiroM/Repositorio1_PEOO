dd659d4593d4d0b20f82ceb4c054b62585bfb012
ArthurPinheiroM <161068806+ArthurPinheiroM@users.noreply.github.com>
2024-07-05 18:52:26 +0000

    mensagem

--- dia 05.07.24 (pos-greve)/posgreve.py
+++ dia 05.07.24 (pos-greve)/posgreve.py
@@ -1,13 +1,25 @@
-import math
-class circulo:
-    def __init__(self):
-        self.raio = 0
-    def area(self):
-        return self.raio**2 * math.pi
-    def circu(self):
-        return 2*self.raio * math.pi
+# import math
+# class circulo:
+#     def __init__(self):
+#         self.raio = 0
+#     def area(self):
+#         return self.raio**2 * math.pi
+#     def circu(self):
+#         return 2*self.raio * math.pi
 
-ct = circulo()
-ct.raio = float(input("Qual o raio do circulo?"))
-print(f"A circuferencia é:{ct.circu()}")
-print(f"A area é:{ct.area()}")
+# ct = circulo()
+# ct.raio = float(input("Qual o raio do circulo?"))
+# print(f"A circuferencia é:{ct.circu()}")
+# print(f"A area é:{ct.area()}")
+
+
+class viagem:
+    def __init__(self):
+        self.diskm = 0
+        self.thor = 0
+        self.tmin = 0
+    def vel_media(self):
+        mineh = self.tmin / 60
+        mineh + self.thor
+
+        
